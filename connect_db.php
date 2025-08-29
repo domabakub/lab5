de97ce44078b5c10ed8bde61f694208e60fc160e
parich <domzabakub789@gmail.com>
2025-08-29 16:18:10 +0700

    dom_db

---
+++ connect_db.php
@@ -0,0 +1,13 @@
+<?php
+$severname = "localhost";
+$username = "parich";
+$password = "6042";
+$dbname = "parich";
+
+$conn = mysqli_connect($severname, $username, $password, $dbname);
+
+// if(!$conn) {
+//     die("Connection failed: ".mysqli_connect_error());
+// }
+// echo "Connected successfully<br>";
+?>
