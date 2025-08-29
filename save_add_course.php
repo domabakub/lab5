24dee57ea9c25a5391e08e2a443a721f74c51ff4
llualldom <domzabakub789@gmail.com>
2025-08-29 23:59:18 +0700

    Add course

--- save_add_course.php
+++ save_add_course.php
@@ -1,11 +1,11 @@
-<?php include("home.php"); ?>
+
 <?php
 require("connect_db.php");
 $course_code = $_POST["course_code"];
 $course_name = $_POST["course_name"];
 $credit = $_POST["credit"];
 $sql ="INSERT INTO courses(course_code, course_name, credit) VALUES('$course_code',
-'$course_name', $credit)'";
+'$course_name', $credit)";
 mysqli_query($conn, $sql);
 header("location: course_list.php");
 exit(0);
