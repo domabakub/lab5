34da197ce051c4951b846771757f4eb8e149cad7
llualldom <domzabakub789@gmail.com>
2025-08-30 14:31:55 +0700

    image in home page

--- home.php
+++ home.php
@@ -1,7 +1,7 @@
 <?php
 echo "<center>";
 echo "<table border=1 width=15%>";
-echo "<td><a href='home.php'.>Home</a></td>";
+echo "<td><a href='home2.php'.>Home</a></td>";
 echo "<td><a href='student_list.php'.>Student</a></td>";
 echo "<td><a href='course_list.php'.>Courses</a></td>";
 echo "<td><a href='exam_result.php'.>Exam Result</a></td>";
---
+++ home2.php
@@ -0,0 +1,5 @@
+<?php
+include("home.php");
+echo "<center>";
+echo "<br>";
+echo "<img src='https://www.insightsintoimpact.com/wp-content/uploads/2018/01/database-graphic-hi-res.jpg' width=50%>";
