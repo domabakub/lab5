c3ea0c0fae2e0666d45809815244ff282e37e6a3
parich <domzabakub789@gmail.com>
2025-09-12 16:09:24 +0700

    finished

--- save_add_student.php
+++ save_add_student.php
@@ -1,11 +1,10 @@
 <?php
 require("connect_db.php");
-$student_code_edit = $_POST["student_code_edit"];
 $student_code = $_POST["student_code"];
 $student_name = $_POST["student_name"];
 $gender = $_POST["gender"];
-$sql ="UPDATE students SET student_code='$student_code', student_name='$student_name',
-gender='$gender' WHERE student_code='$student_code_edit'";
+$sql = "INSERT INTO students (student_code, student_name, gender) 
+        VALUES ('$student_code', '$student_name', '$gender')";
 mysqli_query($conn, $sql);
 header("location: student_list.php");
 exit(0);
